58b1ceec3509bd318e31718296879a72fe856f6d
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-11-18 19:47:14 +0530

    feat: create book impl

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -15,7 +16,7 @@ type Repository struct{
 }
 
 //MARK: Routes
-func (r * Repository) SetupRoutes(app *fiber.App){
+func (r *Repository) SetupRoutes(app *fiber.App){
 	api := app.Group("/api")
 	api.Post("/create_books", r.CreateBook)
 	api.Delete("/delete_books/:id", r.DeleteBook)
@@ -23,10 +24,53 @@ func (r * Repository) SetupRoutes(app *fiber.App){
 	api.Get("/books", r.GetBooks)
 }
 
+//MARK: Book struct
 type Book struct{
 	Author			string		`json:"author"`
 	Title			string		`json:"title"`
 	Publisher		string		`json:"publisher"`
+}
+
+//MARK: CreateBook
+func (r *Repository) CreateBook(c *fiber.Ctx) error{
+	book := Book{}
+
+	err := c.BodyParser(&book)
+
+	if err != nil {
+		c.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message":"request failed"})
+		return err
+	}
+
+	err = r.DB.Create(&book).Error
+
+	if err != nil {
+		c.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message":"an error occurred while creating the book"})
+		return err
+	}
+
+	c.Status(http.StatusOK).JSON(
+		&fiber.Map{"message":"book created successfully", "data":book})
+
+	return nil
+
+}
+
+//MARK: DeleteBook
+func (r *Repository) DeleteBook (c *fiber.Ctx) error{
+
+}
+
+//MARK: GetBookById
+func (r *Repository) GetBookById(c *fiber.Ctx) error{
+
+}
+
+//MARK: Get All Books
+func (r *Repository) GetBooks(c *fiber.Ctx) error{
+
 }
 
 //MARK: Main
